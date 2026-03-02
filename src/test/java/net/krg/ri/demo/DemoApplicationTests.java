4a3dbcc6d6655b3a1d7b537d605ccadc76e2ba78
Nicklas Bekkevold <44287251+nicklasbekkevold@users.noreply.github.com>
2026-03-02 21:35:44 +0100

    Boot Spring application

---
+++ src/test/java/net/krg/ri/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.krg.ri.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
